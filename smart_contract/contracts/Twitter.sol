1d3c7819219a2cbe8a1cec57680d7fbcf5fc429a
Noor Mohammed B <noormohammed.b@gpckasaragod.ac.in>
2022-03-03 22:44:29 +0530

    List User Tweet
    removed user address from Tweet struct
    commented complicated codes

--- smart_contract/contracts/Twitter.sol
+++ smart_contract/contracts/Twitter.sol
@@ -14,7 +14,7 @@ contract Twitter {
         string content;
         uint256 timestamp;
         uint256 userId;
-        address userAddress;
+        // address userAddress;
     }
 
     struct TweetComment {
@@ -28,9 +28,9 @@ contract Twitter {
     mapping(address => uint256[]) public tweetIndex;
     mapping(address => User) public users;
     address[] public userAddressList;
-    uint256 globalUserId = 1;
+    uint256 public globalUserId = 1;
     Tweet[] public tweets;
-    uint256 globalTweetId = 0;
+    uint256 public globalTweetId = 0;
 
     function signup(string memory _name, string memory _url)
         external
@@ -38,10 +38,7 @@ contract Twitter {
     {
         require(!users[msg.sender].isActive, "you are alredy signed up");
         require(bytes(_name).length > 0, "Please write name before submit");
-        require(
-            bytes(_url).length > 0,
-            "Please upload url and submit link (IFPS)"
-        );
+        require(bytes(_url).length > 0, "Link IFPS Error");
         users[msg.sender] = User({
             userId: globalUserId,
             name: _name,
@@ -69,8 +66,8 @@ contract Twitter {
                 tweetId: globalTweetId++,
                 content: _tweetContent,
                 timestamp: block.timestamp,
-                userId: _userId,
-                userAddress: msg.sender
+                userId: _userId
+                // userAddress: msg.sender
             })
         );
     }
@@ -79,41 +76,60 @@ contract Twitter {
         return tweets;
     }
 
-    event userTweetListEvent(
-        uint256 tweetId,
-        string content,
-        uint256 timestamp,
-        uint256 userId,
-        address userAddress
-    );
-
-    // function listUserTweets() external view returns(Tweet memory){
-    function listUserTweets() external returns (string memory) {
-        // require(users[msg.sender].isActive , "Please signup before list your tweets");
-        uint256[] memory userTweetIndex = tweetIndex[msg.sender];
-        uint256 tweetIndexArrayLenth = getCount(userTweetIndex);
-
-        Tweet[] memory userTweetsArray = new Tweet[](tweetIndexArrayLenth);
-        for (uint256 i = 0; i <= globalTweetId; i++) {
-            uint256 j = userTweetIndex[i];
-            userTweetsArray[i] = tweets[j];
-            emit userTweetListEvent(
-                tweets[j].tweetId,
-                tweets[j].content,
-                tweets[j].timestamp,
-                tweets[j].userId,
-                tweets[j].userAddress
-            );
+    function listUserTweets() external view returns (Tweet[] memory) {
+        uint256[] memory userTweetIndexs = tweetIndex[msg.sender];
+        Tweet[] memory userTweets = new Tweet[](userTweetIndexs.length);
+        for (uint256 i = 0; i < userTweetIndexs.length; i++) {
+            userTweets[i] = tweets[userTweetIndexs[i]];
         }
-
-        return userTweetsArray[0].content;
+        return userTweets;
     }
 
-    function getCount(uint256[] memory array)
-        internal
-        pure
-        returns (uint256 count)
-    {
-        return array.length;
-    }
+    // function listUserTweets() external view returns (Tweet[] memory) {
+    //     // uint256 _userId = users[msg.sender].userId;
+    //     uint256[] memory _tweetIndex = tweetIndex[msg.sender];
+    //     Tweet[] memory _tweets = new Tweet[](_tweetIndex.length);
+    //     for (uint256 i = 0; i < _tweetIndex.length; i++) {
+    //         _tweets[i] = tweets[_tweetIndex[i]];
+    //     }
+    //     return _tweets;
+    // }
+
+    // event UserTweetListEvent(
+    //     uint256 tweetId,
+    //     string content,
+    //     uint256 timestamp,
+    //     uint256 userId
+    //     // address userAddress
+    // );
+
+    // function listUserTweets() external payable returns (Tweet[] memory) {
+    // function listUserTweets() external {
+    // require(users[msg.sender].isActive , "Please signup before list your tweets");
+    // uint256[] memory userTweetIndex = tweetIndex[msg.sender];
+    // uint256 tweetIndexArrayLenth = getCount(tweetIndex[msg.sender]);
+    // Tweet[] memory userTweetsArray = new Tweet[](tweetIndexArrayLenth);
+    // for (uint256 i = 0; i <= globalTweetId; i++) {
+    // uint256 j = tweetIndex[msg.sender][i];
+    // userTweetsArray[i] = tweets[j];
+    // uint256 j = 0;
+    // emit UserTweetListEvent(
+    //     tweets[j].tweetId,
+    //     tweets[j].content,
+    //     tweets[j].timestamp,
+    //     tweets[j].userId
+    //     // tweets[j].userAddress
+    // );
+    // }
+
+    // return userTweetsArray;
+    // }
+
+    // function getCount(uint256[] memory array)
+    //     internal
+    //     pure
+    //     returns (uint256 count)
+    // {
+    //     return array.length;
+    // }
 }
