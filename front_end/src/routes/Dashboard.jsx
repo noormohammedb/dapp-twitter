0d82492e36070baa0a3d98f33215a5d74280a460
Noor Mohammed B <noormohammed.b@gpckasaragod.ac.in>
2022-03-09 12:42:32 +0530

    List all tweets in Dashboard
    converted block time stamp to js date
    parsed bigNumber in to numbers

--- front_end/src/routes/Dashboard.jsx
+++ front_end/src/routes/Dashboard.jsx
@@ -1,20 +1,60 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { ethers } from "ethers";
 import { useMyState, setMyState } from "../StatesContext";
 
 const Dashboard = () => {
-  const { account } = useMyState();
+  const { account, myContractProvider } = useMyState();
   const { setAccount } = setMyState();
+  const [tweets, setTweets] = useState([]);
+
+  useEffect(async () => {
+    console.log("Dashboard");
+    console.log("account: ", account);
+
+    const myTweets = await myContractProvider.listAllTweets();
+    console.log("myTweets: ", myTweets);
+
+    myTweets.forEach(async (singleTweet, index) => {
+      console.log("singleTweet: ", singleTweet);
+
+      setTweets((prevTweets) => {
+        console.log("singleTweet.content: ", singleTweet.content);
+        let tempTimeStamp = Number(
+          ethers.utils.formatUnits(singleTweet.timestamp, 0) * 1000
+        );
+        return [
+          ...prevTweets,
+          {
+            tweetId: ethers.utils.formatUnits(singleTweet.tweetId, 0),
+            content: singleTweet.content,
+            timeStamp: tempTimeStamp,
+            userId: ethers.utils.formatUnits(singleTweet.userId, 0),
+            timeStampFormated: new Date(tempTimeStamp).toLocaleString(),
+          },
+        ];
+      });
+    });
+  }, []);
 
   const logoutAccount = () => {
     setAccount();
   };
 
+  console.log("tweets: ", tweets);
+
   return (
     <>
       <div className="">Dashboard</div>
+      {console.log("rendering Dashboard")}
       {account?.length > 0 ? (
         <>
-          <div className="acc">{account}</div>
+          <div className="acc">address : {account}</div>
+          <div className="contract">
+            contract : {myContractProvider?.address}
+          </div>
+          {tweets.map((tweet, index) => (
+            <p key={index}> {JSON.stringify(tweet)} </p>
+          ))}
           <button onClick={logoutAccount}>Logout</button>
         </>
       ) : (
--- front_end/src/utils/getContract.js
+++ front_end/src/utils/getContract.js
@@ -2,9 +2,10 @@ import { ethers } from "ethers";
 import { useMyState, setMyState } from "../StatesContext";
 import { abi } from "../../contractArtifact.json";
 
-// export const ContractAddress = "0x6E3131622c39260330e3c644A4F9a838C6c520aA";
-export const ContractAddress = "0x334F8d9e61fbB163d946a13290CAfEe2B438d9E0";
-export const networkId = 1337;
+// export const ContractAddress = "0x6E3131622c39260330e3c644A4F9a838C6c520aA";   // Ropsten
+export const ContractAddress = "0x334F8d9e61fbB163d946a13290CAfEe2B438d9E0"; // ganache
+export const networkId = 1337; // ganache
+// export const networkId = 3;  // Ropsten
 export default abi;
 
 // export const getContract = () => {
